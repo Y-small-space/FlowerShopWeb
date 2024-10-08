397287487d31e32a428d05866c492bb2b06d0e0a
GJY <jy.small.space@outlook.com>
2024-10-08 22:39:26 +0800

    fix:

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb', // 增大请求体限制
+    },
+  },
 };
 
 export default nextConfig;
